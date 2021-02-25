b6da5211381936b3d686e2316fdf475939bce251
RovinRover <ltwyr4@mst.edu>
2021-02-24 18:16:25 -0600

    Add hex grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
 import "./App.css";
 import { Canvas } from "react-three-fiber";
 import { Hex } from "./components/Hex";
+import * as Honeycomb from "honeycomb-grid";
 
 export const App = () => {
+  const HexFactory = Honeycomb.extendHex({ size: 1.07 });
+  const GridFactory = Honeycomb.defineGrid(HexFactory);
+  const grid = GridFactory.hexagon({ radius: 4 });
+
   return (
     <div className="App">
-      <Canvas>
+      <Canvas
+        camera={{
+          fov: 75,
+          near: 0.1,
+          far: 1000,
+          position: [0, 0, 12],
+          rotation: [-1, Math.PI / 4, 3],
+        }}
+      >
         <ambientLight />
         <pointLight position={[10, 10, 10]} />
-        <Hex />
+        {grid.map((hex) => {
+          const { x, y } = hex.toPoint();
+          return <Hex position={[x, y, 0]} />;
+        })}
       </Canvas>
     </div>
   );
--- src/components/Hex.tsx
+++ src/components/Hex.tsx
@@ -1,23 +1,31 @@
 import { useRef, useState } from "react";
+import { MeshProps } from "react-three-fiber";
 import type { Mesh } from "three";
 
-export const Hex = () => {
+export const Hex = (props: MeshProps) => {
   const mesh = useRef<Mesh>();
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
 
   return (
     <mesh
+      {...props}
       ref={mesh}
-      position={[0, 0, 0]}
       scale={[1, 1, 1]}
       onClick={(_event) => setActive(!active)}
       onPointerOver={(_event) => setHover(true)}
       onPointerOut={(_event) => setHover(false)}
-      rotation={[Math.PI / 2, Math.PI / 2, 0]}
+      rotation={[Math.PI / 2, 0, 0]}
     >
-      <cylinderBufferGeometry args={[1, 1, 1, 6]} />
-      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+      <cylinderBufferGeometry
+        args={[
+          1, //radiusTop
+          1, //radiusBottom
+          0.2, //height
+          6, //radialSegments
+        ]}
+      />
+      <meshStandardMaterial color={hovered ? "#cd8500" : "#8b5a2b"} />
     </mesh>
   );
 };
